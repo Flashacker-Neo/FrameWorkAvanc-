a792ef084ae40d089259f4d9794194f44874e2c8
Flashacker <maxime3510@gmail.com>
2022-06-21 11:48:13 +0200

    exo2-step-l&f

--- exo2/src/app/app.module.ts
+++ exo2/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome'
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPlusSquare as farPlusSquare } from '@fortawesome/free-regular-svg-icons';
+import { faStackOverflow, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +15,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+constructor(library: FaIconLibrary) {
+// Add an icon to the library for convenient access in other components
+library.addIcons(faPlusSquare,
+farPlusSquare,
+faStackOverflow,faGithub,faMedium);
+}
+}
